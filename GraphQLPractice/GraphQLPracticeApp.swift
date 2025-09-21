29578a2f2ce364348511f5cc6b5ccba321607d90
Rin-t <rin.t.4921@gmail.com>
2025-09-21 15:47:59 +0900

    Initial Commit

---
+++ GraphQLPractice/GraphQLPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLPracticeApp.swift
+//  GraphQLPractice
+//
+//  Created by Rin on 2025/09/21.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GraphQLPracticeTests/GraphQLPracticeTests.swift
@@ -0,0 +1,16 @@
+//
+//  GraphQLPracticeTests.swift
+//  GraphQLPracticeTests
+//
+//  Created by Rin on 2025/09/21.
+//
+
+import Testing
+
+struct GraphQLPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
